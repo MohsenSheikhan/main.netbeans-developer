ac64b4d3c899f22a62b57a0f8c9b4fb955713611
mohsen <sheikhancs@gmail.com>
2024-07-20 19:21:35 +0330

    Java cod add

---
+++ createclock/src/main/java/com/mycompany/createclock/Createclock.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.createclock;
+
+/**
+ *
+ * @author mohsen
+ */
+public class Createclock {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
