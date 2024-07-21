ff8d8b22599f82dd4e46ff657767b68f8809c35c
mohsen <sheikhancs@gmail.com>
2024-07-21 19:06:43 +0330

    job

--- javanewcod-class/JavaNewClockj/src/main/java/com/mycompany/javanewclockj/JavaNewClockj.java
+++ javanewcod-class/JavaNewClockj/src/main/java/com/mycompany/javanewclockj/JavaNewClockj.java
@@ -11,6 +11,6 @@ package com.mycompany.javanewclockj;
 public class JavaNewClockj {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("My name is Mohsen");
     }
 }
